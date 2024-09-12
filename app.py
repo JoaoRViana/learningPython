d7f06fa3fa6882275cc41bca994e91e34cbec639
JoaoRViana <joaorviana98@gmail.com>
2024-09-12 09:56:24 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import  customtkinter as ctk
+from tkinter import *
+from tkinter import messagebox
+import openpyxl, xlrd
+import pathlib
+from openpyxl import Workbook
+
+ctk.set_appearance_mode("System");
+ctk.set_default_color_theme("blue");
+
